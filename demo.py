ac960a81ec9ddcc9d194e06e939db96cfd727d4a
bbos1994 <bbos1994@163.com>
2015-10-23 16:05:32 +0800

    Exception in Python

--- demo.py
+++ demo.py
@@ -858,16 +858,112 @@ c = Cat()
 # 	print('finally...')
 # print('END')
 
-try:
-	print('try...')
-	r = 10/int('a')
-	print('result:',r)
-except ValueError as e:
-	print('ValueError:',e)
-except ZeroDivisionError as e :
-	print('ZeroDivisionError:',e)
-else:
-	print('NO ERROR!')
-finally:
-	print('finally...')
-print('END')
+# try:
+# 	print('try...')
+# 	r = 10/int('a')
+# 	print('result:',r)
+# except ValueError as e:
+# 	print('ValueError:',e)
+# except ZeroDivisionError as e :
+# 	print('ZeroDivisionError:',e)
+# else:
+# 	print('NO ERROR!')
+# finally:
+# 	print('finally...')
+# print('END')
+
+# def foo:
+# 	r = some_function()
+# 	if r == (-1):
+# 		return (-1)
+# 	return r
+
+# def bar():
+# 	r = foo()
+# 	if r == (-1):
+# 		print('Error')
+# 	else 
+# 		pass
+
+# try:
+# 	print('try...')
+# 	r = 10 / 1
+# 	print('result:',r)
+# except ZeroDivisionError as e:
+# 	print('except:',e)
+# finally:
+# 	print('finally')
+# print('END')
+
+# try:
+# 	print('try...')
+# 	r = 10 / int('a')
+# 	print('result:',r)
+# except ValueError as e:
+# 	print('ValueError:',e)
+# except ZeroDivisionError as e:
+# 	print('ZeroDivisionError:',e)
+# finally:
+# 	print('finally')
+# print('END')
+
+# try:
+# 	print('try...')
+# 	r = 10 / int('2')
+# 	print('result:',r)
+# except ValueError as e:
+# 	print('ValueError:',e)
+# except ZeroDivisionError as e:
+# 	print('ZeroDivisionError:',e)
+# else :
+# 	print('no error!')
+# finally:
+# 	print('finally...')
+# print('END')
+
+# try:
+# 	foo()
+# except ValueError as e:
+# 	print('ValueError')
+# except UnicodeError as e:
+# 	print('UnicodeError')
+
+#err.py:
+# def foo(s):
+# 	return 10 /int(s)
+
+# def bar(s):
+# 	return foo(s) * 2
+
+# def main():
+# 	bar('0')
+
+# print(main())
+
+# import logging
+
+# def foo(s):
+# 	return 10/int(s)
+
+# def bar(s):
+# 	return foo(s) * 2
+
+# def main():
+# 	try:
+# 		bar('0')
+# 	except Exception as e:
+# 		logging.exception(e)
+
+# main()
+# print('END')
+
+class FooError(ValueError):
+	pass
+
+def foo(s):
+	n = int(s)
+	if n == 0:
+		raise FooError('invalid value:%s'%s)
+	return 10 / n
+
+foo('0')
