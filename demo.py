55456616a06ed5c179264d24b46b434ee4433e04
bbos1994 <bbos1994@163.com>
2015-11-12 12:31:59 +0800

    Learning IO in python

--- demo.py
+++ demo.py
@@ -1007,49 +1007,139 @@
 # pdb.set_trace()
 # print(10/n)
 
-class Dict(dict):
-	def __init__(self,**kw):
-		super().__init__(**kw)
+# class Dict(dict):
+# 	def __init__(self,**kw):
+# 		super().__init__(**kw)
 
-	def __getattr__(self,key):
-		try:
-			return self[key]
-		except KeyError:
-			raise AttributeError(r"'Dict' object has no attirbute '%s'" %key)
-	def __setattr__(self,key,value):
-		self[key] = value
+# 	def __getattr__(self,key):
+# 		try:
+# 			return self[key]
+# 		except KeyError:
+# 			raise AttributeError(r"'Dict' object has no attirbute '%s'" %key)
+# 	def __setattr__(self,key,value):
+# 		self[key] = value
 
 
-#unit test
-import unittest
-from demo import Dict
-class TestDict(unittest.TestCase):
-	def test_init(self):
-		d = Dict(a= 1,b = 'test')
-		self.assertEqual(d.a,1)
-		self.assertEqual(d.b,'test')
-		self.assertTrue(isinstance(d,dict))
+# #unit test
+# import unittest
+# from demo import Dict
+# class TestDict(unittest.TestCase):
+# 	def test_init(self):
+# 		d = Dict(a= 1,b = 'test')
+# 		self.assertEqual(d.a,1)
+# 		self.assertEqual(d.b,'test')
+# 		self.assertTrue(isinstance(d,dict))
 
-	def test_key(self):
-		d = Dict()
-		d['key'] = 'value'
-		self.assertEqual(d.key,'value')
+# 	def test_key(self):
+# 		d = Dict()
+# 		d['key'] = 'value'
+# 		self.assertEqual(d.key,'value')
 
-	def test_attr(self):
-		d = Dict()
-		d.key = 'value'
-		self.assertTrue('key'in d)
-		self.assertEqual(d['key'],'value')
+# 	def test_attr(self):
+# 		d = Dict()
+# 		d.key = 'value'
+# 		self.assertTrue('key'in d)
+# 		self.assertEqual(d['key'],'value')
 
-	def test_keyerror(self):
-		d = Dict()
-		with self.assertRaises(KeyError):
-			value = d['empty']
+# 	def test_keyerror(self):
+# 		d = Dict()
+# 		with self.assertRaises(KeyError):
+# 			value = d['empty']
 
-	def test_attrerror(self):
-		d = Dict()
-		with self.assertRaises(AttributeError):
-			value = d.empty
+# 	def test_attrerror(self):
+# 		d = Dict()
+# 		with self.assertRaises(AttributeError):
+# 			value = d.empty
 	
-if __name__ == '__main__':
-	unittest.main()
+# if __name__ == '__main__':
+# 	unittest.main()
+
+# import re
+# m = re.search('(?<=abc)def','abcdef')
+# print(m.group(0))
+
+# def abs(n):
+# 	'''
+# 	Function to get absolute value of number.
+
+# 	Example:
+
+# 	>>> abs(1)
+# 	1
+# 	>>>abs(-1)
+# 	1
+# 	>>>abs(0)
+# 	0
+# 	'''
+# 	return n if n >= 0 else (-n)
+
+# class Dict(dict):
+# 	'''
+# 	Simple dict but also support access as x.y style.
+
+# 	>>> d1 = Dict()
+# 	>>> d1['x'] = 100
+# 	>>> d1.x
+# 	100
+# 	>>> d1.y = 200
+# 	>>> d1['y']
+# 	200
+# 	>>> d2 = Dict(a=1,b=2,c='3')
+# 	>>> d2.c
+# 	'3'
+# 	>>> d2['empty']
+# 	Traceback (most recent call last):
+# 		...
+# 	KeyError:'empty'
+# 	>>> d2.empty
+# 	Traceback (most recent call last):
+# 		...
+# 	AttributeError:'Dict' object has no attribute 'empty'
+# 	'''
+
+# 	def  __init__(self,**kw):
+# 		super(Dict,self).__init__(**kw)
+
+# 	def __getattr__(self,key):
+# 		try:
+# 			return self[key]
+# 		except KeyError:
+# 			raise AttributeError(r"'Dict' object has no attribute '%s'"% key)
+# 	def __setattr__(self,key,value):
+# 		self[key] = value
+
+# if __name__ == '__main__':
+# 		import doctest
+# 		doctest.testmod()
+
+# try:
+# f = open('../../Desktop/Remarks','r')
+# 	print(f.read())
+# finally:
+# 	if f:
+# 		f.close()
+
+# with open('../../Desktop/Remarks','r') as f:
+# 	print(f.read())
+
+# for line in f.readlines():
+# 	print(line.strip())
+
+# f = open('demo.txt','w')
+# f.write('HelloWorld')
+# f.close()
+# print(f.read())
+
+# f = StringIO()
+# f.write('hello')
+# f.write('  ')
+# f.write('world')
+# print(f.getvalue())
+
+from io import StringIO
+f = StringIO('Hello!\nHi!\nGoodBye!')
+while True:
+	s = f.readline()
+	if s == '':
+		break
+	print(s.strip())
