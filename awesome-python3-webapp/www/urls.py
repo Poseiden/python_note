2ae8a4595b3b3890634ca8f2bea4a194f6f73d49
bbos1994 <bbos1994@163.com>
2016-03-04 13:50:59 +0800

    Day09

--- awesome-python3-webapp/www/urls.py
+++ awesome-python3-webapp/www/urls.py
@@ -6,10 +6,28 @@ import logging
 
 from transwarp.web import get,view
 
+from apis import api,APIValueError,APIPermeissionError,APIResourceNotFoundError
+
 from models import User,Blog,Comment
 
-@view('test_users.html')
+# @view('test_users.html')
+# @get('/')
+# def test_users():
+# 	users = user.find_all()
+# 	return dict(users = users)
+
+@view('blog.html')
 @get('/')
-def test_users():
-	users = user.find_all()
-	return dict(users = users)
+def index():
+	blogs = Blog.find_all()
+	user = User.find_first('where email = ?','admin@example.com')
+	return dict(blogs = blogs,user = user)
+
+@api
+@get('/api/users')
+def api_get_users():
+	users = User.find_by('order by created_at desc')
+	for u in users:
+		u.password = '******'
+	return dict(user = users)
+
