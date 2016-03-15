7f1dd54a4a02265ebdde79c2910311e003d9867a
bbos1994 <bbos1994@163.com>
2016-03-15 22:18:31 +0800

    Day14

--- awesome-python3-webapp/www/models.py
+++ awesome-python3-webapp/www/models.py
@@ -9,8 +9,11 @@ import time,uuid
 from transwarp.db import next_id
 from transwarp.orm import Model,StringField,BooleanField,FloatField,TextField
 
+def next_id():
+	return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
 class User(Model):
-	__table__ = 'blogs'
+	__table__ = 'users'
 
 	id = StringField(primary_key = True,default = next_id,ddl = 'varchar(50)')
 	user_id = StringField(updatable = False,ddl= 'varchar(50)')
